fdd9b53ca0fc4f60323c8b35ecdc13d792db86e1
Dmitrii Baklai <d.n.baklai@gmail.com>
2025-05-31 11:11:23 +0300

    feat: Добавить поддержку ввода API ключа и модели для GPT Agent в компоненте AppConfig

--- src/renderer/components/AppConfig.vue
+++ src/renderer/components/AppConfig.vue
@@ -1,5 +1,7 @@
 <script setup>
-import { computed } from 'vue';
+import { ref, computed } from 'vue';
+
+import AppConnect from '@/components/AppConnect.vue';
 import { useAppStore } from '@/stores/appstore';
 
 const appStore = useAppStore();
@@ -10,6 +12,18 @@ defineEmits(['update:visible']);
 const isInitAgent = computed(() => {
   return appStore.isOnLine && appStore.isAgent;
 });
+
+const models = ref([
+  { name: 'gpt-3.5-turbo', value: 'gpt-3.5-turbo' },
+  { name: 'gpt-4', value: 'gpt-4' },
+  { name: 'gpt-4o', value: 'gpt-4o' }
+]);
+
+const handleContinue = async () => {
+  if (appStore.apiKey && appStore.apiModel) {
+    await appStore.connect();
+  }
+};
 
 const toggleTheme = event => {
   appStore.appTheme = event.value;
@@ -38,9 +52,42 @@ const handleReset = async () => {
     <Divider class="m-0! mb-8!" />
 
     <div class="flex flex-col gap-y-4 px-4">
-      <div class="flex flex-col gap-y-2" v-if="isInitAgent">
+      <div class="flex flex-col gap-y-2">
         <p>GPT Agent</p>
-        <Button severity="contrast" label="Disconnect" class="my-2 w-full" @click="handleReset" />
+        <div class="flex flex-col items-center justify-center gap-y-4" v-if="!isInitAgent">
+          <FloatLabel variant="in" class="w-full">
+            <InputText id="api-key" v-model="appStore.apiKey" variant="filled" class="w-full" />
+            <label for="api-key">OpenAI API Key</label>
+          </FloatLabel>
+
+          <FloatLabel class="w-full" variant="in">
+            <Select
+              v-model="appStore.apiModel"
+              inputId="in_label"
+              :options="models"
+              optionLabel="name"
+              optionValue="value"
+              class="w-full"
+              variant="filled"
+            />
+            <label for="in_label">OpenAI API Model</label>
+          </FloatLabel>
+
+          <Button
+            size="large"
+            severity="secondary"
+            label="Continue"
+            class="my-2 w-full"
+            @click="handleContinue"
+          />
+        </div>
+        <Button
+          severity="contrast"
+          label="Disconnect"
+          class="my-2 w-full"
+          @click="handleReset"
+          v-else
+        />
       </div>
 
       <div class="flex flex-col gap-y-2">
